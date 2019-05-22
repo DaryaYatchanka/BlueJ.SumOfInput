117a3ede2104a17691a705432c84ac1a8d09c0d5
Darya <Darya.yatchanka@gmail.com>
2019-05-22 13:31:46 -0400

    SumOfInput done

--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
-
+     
     public static void main(String[] args){
-
-    }
+     int sum = 0;
+     Scanner sc = new Scanner(System.in);
+     int n = 0;
+     n = sc.nextInt();
+      for (int i=1;i <=n;i++){
+          sum += i;
+        
+          
+        
+        
+      }     
+     System.out.println (sum);
+    } 
+   
 }
